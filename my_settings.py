6f41c76895a39d63cb1d52aa7d90c9b0ae177e13
DaeHyun Kim <denniskim@DaeHyuns-MacBook-Pro.local>
2022-03-11 20:19:06 +0900

    Add; Westarbucks Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks_db',
+        'USER': 'root',
+        'PASSWORD': 'rlaeogus0116',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-5*)%2*+03lh8%i)sc8023@790pom!y(+dmp4ogozqz=4o$3-s)'
